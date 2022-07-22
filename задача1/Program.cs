3352299f0f1b9b430b4172ee593dd4e79ed54000
Katya Panko <katena-1990@mail.ru>
2022-07-22 15:25:07 +0300

    задача1

---
+++ задача1/Program.cs
@@ -0,0 +1,17 @@
+﻿/*  Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+M = 4; N = 8. -> ""4, 6, 7, 8""*/
+
+string recursion(int m, int n)
+{
+    if(n == m)
+        return m.ToString();
+    else
+        return m + ", " + recursion(m+1,n) ;
+}
+Console.WriteLine("введите число n");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("введите число m");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{recursion(m,n)} ");
